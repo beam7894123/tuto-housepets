275ff7857d6c97a3173287f26331baa3fed26e0a
beam7894123 <beamtamamoon@gmail.com>
2021-12-13 18:31:51 +0700

    disable ad 'w'b

--- content-header.php
+++ content-header.php
@@ -14,24 +14,14 @@
 		<?php tuto_custom_header(); ?>
 	</div>
 	
-<!-- AD  (I am sorry >~<) -->
+<!-- AD  (I am sorry >~<) now is disable 'w'b
 		<div class="widget_text mh-widget-inner"><h4 class="mh-widget-title"><span class="mh-widget-title-inner mh-sidebar-widget-title-inner">โฆษณา (AD)</span></h4>
 				<div align="center">
-					<script type="text/javascript">
-						atOptions = {
-						'key' : '2616e2fed829d833f90a35221632c662',
-						'format' : 'iframe',
-						'height' : 90,
-						'width' : 728,
-						'params' : {}
-											};
-						document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variouscreativeformats.com/2616e2fed829d833f90a35221632c662/invoke.js"></scr' + 'ipt>'); 
-					</script>
-				</div>
-			</div>
+				> AD go here! <
+			</div>  
 	<div>
 <h4 class="mh-widget-title"><span class="mh-widget-title-inner mh-sidebar-widget-title-inner"></span></h4>
-	</div>
+	</div> -->
 <!-- AD END (I dont want to add this but to keep this site up I need moneyyy ;^; ) -->
 	
 	<div class="mh-main-nav-wrap clearfix">
